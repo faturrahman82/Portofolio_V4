420d1d82e0ae0d1024a87c65a4486767a90f1d60
fatrurrahman82 <newaqount99@gmail.com>
2026-04-16 23:15:55 +0700

    fix github

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -137,12 +137,12 @@ export async function generateMetadata({
     icons: {
       icon: [
         { url: '/favicon.ico', sizes: 'any' },
-        { url: '/icon.svg', type: 'image/svg+xml' },
-        { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-        { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+        // { url: '/icon.svg', type: 'image/svg+xml' },
+        // { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+        // { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
       ],
-      apple: [{ url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }],
-      other: [{ rel: 'mask-icon', url: '/safari-pinned-tab.svg', color: '#00f5ff' }],
+      // apple: [{ url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }],
+      // other: [{ rel: 'mask-icon', url: '/safari-pinned-tab.svg', color: '#00f5ff' }],
     },
     other: {
       'msapplication-TileColor': '#050816',
